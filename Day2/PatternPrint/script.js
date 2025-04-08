f5736c589407cdfd594777d688ab79aa604f2da3
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-08 21:37:31 +0530

    [JS-Practice] [Day 2] [PatternPrint] TrianglePattern2 solved

--- Day2/PatternPrint/script.js
+++ Day2/PatternPrint/script.js
@@ -6,16 +6,35 @@
 // 1 2 3 4
 // 1 2 3 4 5
 
-const TrianglePattern1 = () => {
-    let n = 5;
-    let pattern = "";
-    for (let i = 1; i <= n; i++) {
-        for (let j = 1; j <= i; j++) {
-            pattern = pattern + j + " ";
-        }
-        pattern = pattern + "\n";
-    }
-    console.log(pattern);
-}
+// const TrianglePattern1 = () => {
+//     let n = 5;
+//     let pattern = "";
+//     for (let i = 1; i <= n; i++) {
+//         for (let j = 1; j <= i; j++) {
+//             pattern = pattern + j + " ";
+//         }
+//         pattern = pattern + "\n";
+//     }
+//     console.log(pattern);
+// }
 
-TrianglePattern1(5);
+// TrianglePattern1(5);
+
+// -------------------------------------------------------------------------------------
+// Triangle Pattern — II
+
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+
+let n = 5;
+let pattern = "";
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+        pattern = pattern + i + " ";
+    }
+    pattern = pattern + "\n";
+}
+console.log(pattern);
