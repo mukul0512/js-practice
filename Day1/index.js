bc1a8da5d74880268d0ece8127c24aca0072aab9
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-08 17:39:25 +0530

    [JS-Practice] [Day 1] did basic css, html, js practice

---
+++ Day1/index.js
@@ -0,0 +1,15 @@
+let box1 = "I'm box 1";
+
+setTimeout(() => {
+    document.querySelector(".box1").innerHTML = "Hey"
+}, 1000);
+
+let box2 = "I'm box 2";
+setTimeout(() => {
+    document.querySelector(".box2").innerHTML = "Hi"
+}, 2000);
+
+let box3 = "I'm box 3";
+setTimeout(() => {
+    document.querySelector(".box3").innerHTML = "Hello"
+}, 3000);
