5ddd145a9c134febc8c7ba0611c9d81e805733e3
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-09 20:53:05 +0530

    [JS-Practice] [Day 4] Calculator prog. using if ...else

--- Day4/script.js
+++ Day4/script.js
@@ -32,3 +32,35 @@ console.log(calculator(8, 2, '*')); // 16
 console.log(calculator(8, 2, '/')); // 4
 console.log(calculator(8, 0, '/')); // Error: Division by zero
 console.log(calculator(8, 2, '%')); // Error: Invalid operator
+
+// ------------------------------------------------------------------------------------------
+
+// Calculator using if ...else
+const calculate = (a, b, operator) => {
+    let result;
+
+    if (operator === '+') {
+        result = a + b;
+    } else if (operator === '-') {
+        result = a - b;
+    } else if (operator === '*') {
+        result = a * b;
+    } else if (operator === '/') {
+        if (b !== 0) {
+            result = a / b;
+        } else {
+            result = 'Error: Division by zero';
+        }
+    } else {
+        result = 'Error: Invalid operator';
+    }
+
+    return result;
+};
+
+console.log(calculate(12, 4, '+')); // 16
+console.log(calculate(12, 4, '-')); // 8
+console.log(calculate(12, 4, '*')); // 48
+console.log(calculate(12, 4, '/')); // 3
+console.log(calculate(12, 0, '/')); // Error: Division by zero
+console.log(calculate(12, 4, '%')); // Error: Invalid operator
