63b844cbf0549944e77801a288c51e27a02ccf36
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-09 22:59:54 +0530

    [JS-Practice] [Day 4] [Practice] Variables, data types, and objects

--- Day4/Practice/script.js
+++ Day4/Practice/script.js
@@ -17,3 +17,46 @@
 
 // document.title = "Fundamentals";
 // document.body.style.backgroundColor = "red";
+
+// -------------------------------------------------------------------------------------------
+
+// Variables, data types, and objects
+// var a = 5;
+// a = a + 1; // Allowed in case of var
+
+// var b = "Mukul";
+// var c = 5.8;
+// console.log(typeof a, typeof b, typeof c);
+
+// var 55a = "Mukul";
+// console.log(55a);  // SyntaxError: Invalid or unexpected token
+
+// const a1 = 6;
+// a1 = a1 + 1;  // TypeError: Assignment to constant variable.
+
+// ------------------------------------------------------------------
+// let x = "Mukul bhai";
+// let y = 23;
+// let z = 3.55;
+// const p = true;
+// let q = undefined;
+// let r = null;  // object type
+
+// console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+// --------------------------------------------------------------------
+
+// Objects
+// let o = {
+//     name: "Mukul bhai",
+//     "job role": "Full Stack Dev",
+//     "is_handsome": true
+// }
+// console.log(o);
+// o.salary = "100crores";
+// console.log(o);
+
+// o.salary = "500crores";
+// console.log(o);
+
+// ----------------------------------------------------------------
