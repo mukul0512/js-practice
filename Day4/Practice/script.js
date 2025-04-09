db69d8d21035bf9a72b76c6aa41e487ed5745b8e
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-09 22:15:05 +0530

    [JS-Practice] [Day 4] [Practice] print hello world, console, run js in browser, prompt usecase, confirm usecase

---
+++ Day4/Practice/script.js
@@ -0,0 +1,17 @@
+alert("Hello World");
+
+console.log("Code is running...");
+console.log("Code is also running...");
+console.log("Code is looking like a wow...");
+
+var a = prompt("Enter your number");
+var isTrue = confirm("Are you sure you want to leave this page and blast your pc ");
+if (isTrue) {
+    console.log("Computer is blasting");
+} else {
+    console.log("Computer is not blasting");
+}
+console.log("Your number is " + a);
+
+// -----------------------------------------------------------------------------------------
+
