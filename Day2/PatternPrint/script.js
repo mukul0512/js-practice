d7e368aa795faf43b22fd476ed09b645fb38b98a
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-10 20:18:18 +0530

    [JS-Practice] [Day 2] [PatternPrint] RightTrianglePattern solved

--- Day2/PatternPrint/script.js
+++ Day2/PatternPrint/script.js
@@ -195,3 +195,31 @@ const HollowSquareStarPatter = (n) => {
     console.log(pattern);
 }
 HollowSquareStarPatter(5);
+
+// -------------------------------------------------------------------------------------
+
+// 9. Right Triangle Pattern
+/*
+
+    *
+   **
+  ***
+ ****
+*****
+
+*/
+
+const RightTrianglePattern = (n) => {
+    let pattern = "";
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= n - i; j++) {
+            pattern = pattern + " ";
+        }
+        for (let k = 1; k <= i; k++) {
+            pattern = pattern + "*";
+        }
+        pattern = pattern + "\n";
+    }
+    console.log(pattern);
+}
+RightTrianglePattern(5);
