e39a888f59d9c7598f95037abf418817bbdc66a4
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-11 19:52:04 +0530

    [JS-Practice] [Day 3] function declaration, arrow, anonymous, scop, factorial, fibonacci, call by value, reference loop over object

--- Day3/script.js
+++ Day3/script.js
@@ -1,116 +1,105 @@
 // difference btw void and undefined and null
 
-// console.log(square(4,5));
-// // 1. function declaration
-// function square(number, x) {
-//     // code
-//     console.log(x);
-//     return;
-// }
-// const result = square(4);
-// console.log(result);
+console.log(square(4, 5));
+// square(4, 5);
+// 1. function declaration
+function square(number, x) {
+    console.log(number, x);
+    return;
+}
+const res = square(4);
+console.log(res);
+console.log(typeof square);
 
-// // 2. arrow function
-// const squareArr = (number) => number * number;
-// console.log(squareArr(4));
+// 2. arrow function
+const squareArr = (number) => number * number;
+console.log(squareArr(4));
 
-// //. 3. anonomous function
+//. 3. anonymous function
+const squareAnonymous = function (number) {
+    return number * number;
+};
+const x = squareAnonymous(4);
+console.log(x);
+squareAnonymous(4);
 
-// const squareAnonomous = function (number) {
-//     return number * number;
-// };
-// const x = squareAnonomous(4);
-// console.log(x);
-// console.log(squareAnonomous(4)); // 16
+if (true) {
+    console.log("hii");
+    console.log('Hello')
+}
 
-// // console.log(typeof square);
-// if(true){
-//     console.log("hii");
-//     console.log('Hello')
-// }
+let name = "Ram";
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    if (true) {
+        var a = 10;
+        let y = 5;
+        let child_Value;
+        console.log(y);
+        if (true) {
+            y = 10;
+            console.log(y);
+            child_Value = 10;
+        }
+        console.log(y);
+        console.log(child_Value);
+    }
+    console.log(a);
+}
+console.log(name);
 
-// let name = "Ram";
-
-// for(let i = 0; i < 10; i++){
-//     console.log(i);
-//     if(true){
-//         var a = 10;
-//         let y = 5;
-//         let child_Value;
-//         // console.log(y);
-//         if(true){
-//             y = 10;
-//             console.log(y);
-//             child_Value = 10;
-//         }
-//         console.log(y);
-//         console.log(child_Value);
-//     }
-//     // console.log(a);
-//     // console.log(y);
-// }
-
-// // console.log(typeof null)
-
+console.log(typeof null)
+// --------------------------------------------------------------------------------
 // /* [1] -> [2] -> [3] ([4])
-//  *  [1] -> [2] -> [3]
-//  *
-//  *  in c++ temp.clear([4])
-//  *
-//  *
-// */
-// console.log(name);
+// [1] -> [2] -> [3]
+//  in c++ temp.clear([4])
+// ------------------------------------------------------------
 
-// //
-// function fact(x){
-//     // base case
-//     if(x === 0 || x === 1){
-//         return 1;
-//     }
-//     // logic
-//     // recursive call
-//     return fact(x-1)*x;
-// }
+function fact(x) {
+    // base case
+    if (x === 0 || x === 1) {
+        return 1;
+    }
+    // logic
+    // recursive call
+    return fact(x - 1) * x;
+}
+console.log("Factorial :", fact(5));
 
-// // console.log("Factioral :", fact(6));
+// -----------------------------------------------------
+function outside() {
+    const s = 5;
+    function inside(s) {
+        return s * 2;
+    }
+    return inside;
+}
+console.log(outside())
+const s = outside();
+console.log(s(5));
+console.log(outside()(1));
+// -------------------------------------------------------------------------
 
+const multiply = (multiplier, ...bakiKaData) => {
+    console.log(bakiKaData);
 
-// function outside() {
-//     const x = 5;
-//     function inside(x) {
-//       return x * 2;
-//     }
-//     return inside;
-//   }
+    for (let i = 0; i < bakiKaData.length; i++) {
+        console.log(bakiKaData[i] * multiplier);
+    }
+    return 5;
+}
+const temp = () => 5;
+console.log(temp());
 
-// //   console.log(outside())
-// const x = outside();
-// console.log(x(5));
-// console.log(outside()(1));
-
-
-
-
-// const multiply = (multiplier, ...bakiKaData) => {
-//     console.log(bakiKaData);
-
-//     for (let i = 0; i < bakiKaData.length; i++) {
-//         console.log(bakiKaData[i] * multiplier);
-//     }
-//     return 5;
-// }
-// const temp = () => 5;
-// console.log(temp());
-
-// const returnedValue = multiply(2, 1, 2, 3, 4, 5, 6, 7)
-// console.log(returnedValue);
-//   const arr = multiply(2, 1, 2, 3);
-//   console.log(arr);
+const returnedValue = multiply(2, 1, 2, 3, 4, 5, 6, 7)
+console.log(returnedValue);
+const arr = multiply(2, 1, 2, 3);
+console.log(arr);
 
 // -------------------------------------------------------------------
 
-let a;
-console.log(a); // undefined
+let g;
+console.log(g); // undefined
 
 let obj = {};
 console.log(obj.name); // undefined
@@ -123,22 +112,18 @@ function changeValue(x) {
     x = x + 10;
     console.log("Inside function:", x);
 }
-
 let num = 5;
 changeValue(num);
-
-console.log("Outside function:", num); // Still 5
+console.log("Outside function:", num);
 
 // call by reference
 function updatePerson(person) {
-    person.age = 30;
+    person.age = 25;
     console.log("Inside function:", person);
 }
-
-let user = { name: "Alice", age: 25 };
+let user = { name: "Mukul", age: 25 };
 updatePerson(user);
-
-console.log("Outside function:", user); // age is now 30
+console.log("Outside function:", user);
 
 // Recursion - Factorial 
 function factorial(n) {
@@ -169,9 +154,9 @@ printFibonacciSeries(10);
 
 // loop to iterate over an object’s keys and values using template literal we can access key and value.
 const person = {
-    name: "Alice",
+    name: "Mukul",
     age: 28,
-    city: "Paris"
+    city: "Haridwar"
 };
 
 for (let key in person) {
