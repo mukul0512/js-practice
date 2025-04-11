ea582a1dae70aa5d665136655cbfa548f298d7da
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-11 12:54:23 +0530

    [JS-Practice] [Day 5] Did closure overview, scope, multiplier code ex, call by reference taking swap ex. inside array, Math.random generate 10 digit no

--- Day5/script.js
+++ Day5/script.js
@@ -27,22 +27,23 @@
 // let randNum = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
 // console.log(randNum);
 
+
 // ------------------------------------------------------------------------
 
 // Recursion - Fibonacci
-// function fibonacci(n) {
-//     if (n <= 1) {
-//         return n;
-//     }
-//     return fibonacci(n - 1) + fibonacci(n - 2);
-// }
+function fibonacci(n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
 
-// function printFibonacciSeries(count) {
-//     for (let i = 0; i < count; i++) {
-//         console.log(fibonacci(i));
-//     }
-// }
-// printFibonacciSeries(7);
+function printFibonacciSeries(count) {
+    for (let i = 0; i < count; i++) {
+        console.log(fibonacci(i));
+    }
+}
+printFibonacciSeries(7);
 
 // ---------------------------------------------------------------------
 
@@ -80,3 +81,129 @@ let obj2 = obj1; // because holds same memory
 obj2.name = "Mukul";
 console.log(obj1);
 console.log(obj2);
+
+// let x = 5;
+// function Parent(){
+//     const name = "Ram";
+//     function Child(){
+//         console.log(name);
+//     }
+//     return Child;
+// }
+// const y = Parent();
+// y()
+
+// const obj = {
+//     name:"ram",
+//     age:25,
+//     address:"Delhi"
+// }
+// const prop = "name";
+// console.log(obj[prop])
+
+// Task 1 
+// function generateRandomNumber(noOfDigit){
+//     const x = Math.random();
+//     const value = x * 10000000000;
+//     return Math.floor(value)
+// }
+
+// console.log(generateRandomNumber(50));
+
+// call by value
+
+// call by reference
+// function swap(a , b , arr) {
+//     let temp = arr[a];
+//     arr[a] = arr[b];
+//     arr[b] = temp;
+// }
+// const array = [1,2];
+// swap(0,1, array);
+// console.log(array[0] +" " + array[1])
+
+// fibonacci series
+
+/**
+ * 0
+ * 1
+ * 1
+ * 2
+ * 3
+ * 5
+ * 8
+ * 13
+ * 
+ */
+// 5
+
+// Fibonacci algorithm
+// step 1 make a function 
+// step 2 base case n <= 1 return n;
+// step 3 return fibonacci(n-1) + fibonacci(n-2)
+
+// function fibonacci(n){
+//     if(n <= 1) return n;
+
+//     return fibonacci(n-1) + fibonacci(n-2);
+// }
+// for(let i=0;i<=5;i++){
+//     const x = fibonacci(i);
+//     console.log(x);
+// }
+// function printFibo(n){
+//     for(let i=0;i<n;i++){
+//         const x = fibonacci(i);
+//         // console.log(x);
+//     }
+// }
+// console.log(printFibo(5));
+
+// Understand variable scope: global scope, function scope, and block scope.
+
+let name = "Ram"; // global scope
+
+function Outer() {
+    let name = "Site";
+    // heavy computaion
+    function Inner() {
+        console.log(name);
+    }
+    return Inner;
+}
+const printName = Outer();
+printName();
+
+
+function createMultiplier(multiplier) {
+    return function (number) {
+        return number * multiplier; // Multiply input number by multiplier
+    };
+
+}
+const child = createMultiplier(5);
+console.log(child(3))
+// console.log(child(4))
+
+
+// function createMultiplier(multiplier) {
+// // Return a new function
+// return function(number) {
+//     return number * multiplier; // Multiply input number by multiplier
+// };
+// }
+
+// const double = createMultiplier(2); // Function to double a number
+// const triple = createMultiplier(3); // Function to triple a number
+
+// console.log(double(5)); // 10
+// console.log(triple(5)); // 15
+
+/**
+
+
+ */
+
+
+// ------------------------------------------------------------------------------------
+
