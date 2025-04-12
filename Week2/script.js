2c8a3394381d1613334ab36609f421c3e6f263ba
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-12 14:19:47 +0530

    [JS-Practice] [Week2] access parents, children, siblings nodes from body and a table

--- Week2/script.js
+++ Week2/script.js
@@ -1,4 +1,47 @@
-document.title = "DOM vs BOM";
+// Dom vs BOM
+console.log(document.title = "DOM vs BOM");
 console.log(document.body);
 console.log(document.body.style.backgroundColor = "green");
+
+// --------------------------------------------------------------
+
+// How do we access Parents, Children, Sibling Nodes 
+console.log(document.body.childNodes[0]);  //  #text
+console.log(document.body.childNodes[1]);  // div.container
+console.log(document.body.childNodes[1].childNodes[0]); // #text
+let cont = document.body.childNodes[1];
+console.log(cont.firstChild); // #text
+console.log(cont.lastChild); // #text
+console.log(cont.style.backgroundColor = "yellow"); // yellow
+// console.log(cont.firstElementChild.style.backgroundColor = "red"); // red
+// console.log(cont.lastElementChild.style.backgroundColor = "purple"); // purple
+console.log(cont.firstElementChild.childNodes[1].style.backgroundColor = "white"); // white
+console.log(cont.lastElementChild.childNodes[1].style.backgroundColor = "white"); // purple
+// ---------------------------------------------------------------------------------------------
+
+let contChild = document.body.firstElementChild.children;
+console.log(contChild[0].style.backgroundColor = "red");
+console.log(contChild[1].style.backgroundColor = "blue");
+console.log(contChild[2].style.backgroundColor = "purple");
+console.log(contChild[3].style.backgroundColor = "orange");
+console.log(contChild[4].style.backgroundColor = "khaki");
+console.log(contChild[5].style.backgroundColor = "brown");
+console.log(contChild[6].nextElementSibling.style.backgroundColor = "cyan");
+console.log(contChild[7].previousElementSibling.style.backgroundColor = "pink");
+console.log(contChild[7].parentElement.style.backgroundColor = "pink");
+
+// --------------------------------------------------------------------------------------------------------
+
+console.log(document.body.children);
+let conTableChild = document.body.children[1];
+console.log(conTableChild.caption.style.backgroundColor = "purple");
+console.log(conTableChild.style.backgroundColor = "red");
+console.log(conTableChild.rows);
+console.log(conTableChild.rows[0].style.backgroundColor = "khaki");
+console.log(conTableChild.rows[1].style.backgroundColor = "yellow");
+console.log(conTableChild.rows[2].style.backgroundColor = "pink");
+console.log(conTableChild.rows[3].style.backgroundColor = "brown");
+console.log(conTableChild.lastElementChild.style.backgroundColor = "cyan");
+
+// -------------------------------------------------------------------------------------------------------------
 
