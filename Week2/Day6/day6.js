414ccfd1df16dd34c1ee7b4d3cc7de437bbba019
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-14 22:12:04 +0530

    [JS-Practice] [Week2] [Day6] added js to make calculation using eval()

---
+++ Week2/Day6/day6.js
@@ -0,0 +1,10 @@
+document.getElementById('calc').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const num1 = parseInt(document.getElementById('num1').value);
+    const num2 = parseInt(document.getElementById('num2').value);
+    const operation = document.querySelector('select[name="operation"]').value;
+    const res = document.getElementById('res');
+    const calculate = `${num1} ${operation} ${num2}`;
+    const result = eval(calculate);
+    res.value = "Result is: " + result;
+});
