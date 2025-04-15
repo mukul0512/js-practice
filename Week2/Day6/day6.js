2564462ef9e72c877e27bc5df69c9e66ea222b20
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-15 12:04:07 +0530

    [JS-Practice] [Week2] [Day6] without template litrals

--- Week2/Day6/day6.js
+++ Week2/Day6/day6.js
@@ -4,7 +4,11 @@ document.getElementById('calc').addEventListener('submit', function (e) {
     const num2 = parseInt(document.getElementById('num2').value);
     const operation = document.querySelector('select[name="operation"]').value;
     const res = document.getElementById('res');
-    const calculate = `${num1} ${operation} ${num2}`;
+    const calculate = num1 + " " + operation + " " + num2;
+
+    // const calculate = `${num1} ${operation} ${num2}`;
+    console.log(calculate);
+
     const result = eval(calculate);
     res.value = "Result is: " + result;
 });
