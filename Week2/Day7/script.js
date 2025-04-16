67defec55ebed566c149500b9971776d78835a76
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-16 21:46:18 +0530

    [JS-Practice] [Week2] [Day7] bug fix using replace last character with new character

--- Week2/Day7/script.js
+++ Week2/Day7/script.js
@@ -1,22 +1,39 @@
 const output = document.querySelector("output");
 const buttons = document.querySelectorAll("button");
 let currentInput = "";
+
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         const value = button.textContent;
         if (value === "=") {
             try {
-                currentInput = eval(currentInput).toString();
+                const result = eval(currentInput);
+                currentInput = result.toString();
             } catch {
-                alert('Please enter valid expression')
-                output.textContent = ''
-                return
+                currentInput = "0";
             }
         } else if (value === "Del") {
             currentInput = currentInput.slice(0, -1);
+        } else if (checkForOperator(currentInput, value)) {
+            // replace last character with new character 
+            let lastCharacterIndex = currentInput.length - 1
+            let lastCharacter = currentInput[lastCharacterIndex]
+            let newInput = currentInput.replace(lastCharacter, value)
+            currentInput = newInput;
         } else {
             currentInput = currentInput + value;
         }
         output.textContent = currentInput;
     });
 });
+
+function checkForOperator(currentVal, newVal) {
+    const operators = ['+', '-', '*', '/', '%', '(', ')'];
+    if (operators.includes(newVal)) {
+        const lastChar = currentInput[currentVal.length - 1];
+        if (operators.includes(lastChar)) {
+            return true
+        }
+    }
+    return false
+}
