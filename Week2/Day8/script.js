73f3e584cc6b8b8638a852463ad7f92a647a304f
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-16 18:11:40 +0530

    [JS-Practice] [Week2] [Day8] did example of rest and spread

--- Week2/Day8/script.js
+++ Week2/Day8/script.js
@@ -24,9 +24,9 @@ let obj4 = {
     age: 8
 }
 
-function combineAllObject(...objList) {
+function combineAllObject(...objList) {   // rest operator - parameter mai pass kiye hue object ya array k element ko combine karne ka kaam karta hai
     return {
-        ...objList
+        ...objList // spread operator - object ya array k elements ko receive karne ka kaam karta hai in spread form
     }
 }
 
@@ -34,7 +34,7 @@ console.log(combineAllObject(obj1, obj2, obj3, obj4))
 
 // -------------------------------------------------------------------
 
-function add(...z) {
+function add(...z) {  // rest operator 
     let res = 0;
     for (let i = 0; i < z.length; i++) {
         res = res + z[i];
@@ -48,3 +48,70 @@ function printElements(...elementList) {
 printElements(1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
 
 // --------------------------------------------------------------------------------------
+
+
+// rest operator on array
+function addNumbers1(a, b, c) { // without rest operator
+    return a + b + c;
+}
+const result1 = addNumbers1(2, 3, 1);
+console.log(result1);
+// ---------------------------------------------------------------------
+function addNumbers(a, b, c, ...other) { // using rest operator
+    console.log(other);
+    console.log(other[0]);
+    console.log(other[1]);
+    console.log(other[2]);
+    console.log(other[3]);
+    console.log(other[4]);
+    return a + b + c + other; // 6 is the sum od a, b, c and 8, 9, 15, 16 are the other array element
+}
+const result = addNumbers(2, 3, 1, 8, 9, 15, 16);
+console.log(result);
+
+// --------------------------------------------------------------------------------------------------------
+// spread operator example
+
+let name = ["Mukul", "Peeyush", "Pulkit"];
+function getNames(name1, name2, name3) {
+    console.log(name1, name2, name3);
+}
+// getNames(name[0], name[1], name[2]); // without spread
+getNames(...name); // using spread - - elements ko segregate kar sakte hai. Means alag karna.
+// getNames(name); 
+
+// ----------------------------------------------------------------------------
+
+// Rest - Object k saath 
+
+//without rest operator
+// let students = {
+//     myName: "Mukul",
+//     age: 25,
+//     hobbies: ["Programming", "Coding"]
+// }
+
+// // const age = students.age; 
+// const { myName, age } = students; // array destructuring
+// console.log(myName, age);
+
+// ----------------------------------------------------------------------------------------------
+// Rest - Object k saath 
+let students = {
+    myName: "Mukul",
+    age: 25,
+    hobbies: ["Programming", "Coding"]
+}
+
+// const age = students.age; 
+const { myName, ...other } = students; // using rest operator
+console.log(myName, other);
+
+// spread - Object k saath
+// very very very important example 
+let newStudent = {
+    ...students,
+    age: 26
+}
+
+console.log(newStudent);
