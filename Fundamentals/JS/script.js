716e916a23f5ecff5cb6d2f86e36d4526f57593a
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-17 22:55:48 +0530

    [JS-Practice] [Fundamentals] [JS] did arrays examples and loops

--- Fundamentals/JS/script.js
+++ Fundamentals/JS/script.js
@@ -27,3 +27,82 @@ console.log(typeof (profile["username"]));
 
 // ------------------------------------------------------------------------------
 
+let a = "5";
+let b = 5;
+console.log("a == b", a == b); // check for value only
+console.log("a === b", a === b); // check for value and type
+// ---------------------------------------------------------------------------------------
+
+// let num = prompt("Enter no: ");
+// if (num % 5 === 0) {
+//     console.log(`${num} is multiple of 5`);
+// }
+// else {
+//     console.log(`${num} is not the multiple of 5`);
+// }
+
+// --------------------------------------------------------------------------------
+// for ...of loop -> used for strings and arrays
+let str = "Mukul";
+for (let i of str) {
+    console.log("i = ", i);
+}
+// for ...in loop  -> used for objects and arrays
+let stud = {
+    name: "mukul",
+    marks: 75,
+    age: 25,
+    isPass: true
+}
+
+for (let key in stud) {
+    console.log(key, stud[key]);
+}
+
+// ----------------------------------------------------------------------------
+
+// let num = 25;
+// let guessNum = prompt("Guess the correct number ");
+// while (guessNum != num) {
+//     guessNum = prompt("Guess again please ");
+// }
+// console.log("Congratulations! You guessed the correct number ");
+
+// --------------------------------------------------------------------------------
+
+let penObj = {
+    item: "pen",
+    price: 10
+};
+console.log("the cost of", penObj.item, " is", penObj.price, "rupees");
+console.log(`the cost of ${penObj.item} is ${penObj.price} rupees`);
+console.log(`this is a template literal ${1 + 2 + 3}`);
+
+let str1 = "Mukul"
+str1[0] = "V"; // not possible
+str1 = str1.replace(str1[0], "P")
+console.log(str1);
+console.log(str1.replace("M", "V"));
+// --------------------------------------------------------------------------------------------
+
+// let fullname = prompt(`Enter fullname`);
+// let username = "@" + fullname + fullname.length;
+// console.log(username);
+
+// ------------------------------------------------------------------------------------
+
+let items = [500, 200, 50, 350, 450, 700];
+// let i = 0;
+// for (let val of items) {
+//     console.log(`value at index ${i} = ${val}`);
+//     let offer = val / 10;
+//     items[i] = items[i] - offer;
+//     console.log(`value after offer = ${items[i]}`);
+//     i++;
+// }
+
+for (let i = 0; i < items.length; i++) {
+    let offer = items[i] / 10;
+    items[i] -= offer;
+}
+console.log(items);
