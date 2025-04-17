57a337331655994abac101e6a6f4ad807f24e05b
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-17 17:16:48 +0530

    [JS-Practice] [Fundamentals] [JS] did objects examples

---
+++ Fundamentals/JS/script.js
@@ -0,0 +1,29 @@
+const student = {
+    name: "Mukul",
+    age: 25,
+    marks: 68.91,
+    isPass: true
+};
+
+student["age"] = student["age"] + 1;
+console.log(student["age"]);
+
+const product = {
+    name: "Parker Jotter Standard CT Ball Pan (Black)",
+    rating: 4.5,
+    price: "₹270",
+    offer: "5%"
+}
+
+const profile = {
+    username: "@mukul.codinworkx",
+    isFollow: true,
+    followers: 123,
+    following: 123
+}
+
+console.log(typeof (profile));
+console.log(typeof (profile["username"]));
+
+// ------------------------------------------------------------------------------
+
