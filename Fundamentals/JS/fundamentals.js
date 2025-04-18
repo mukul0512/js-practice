1903e9ac049629f7a22ca34173cc4b1257b135c7
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-18 19:32:50 +0530

    [JS-Practice] [Fundamentals] [JS] did for ...loop exaples

--- Fundamentals/JS/fundamentals.js
+++ Fundamentals/JS/fundamentals.js
@@ -1,4 +1,16 @@
 console.log("Let's get started..");
 
 // --------------------------------------------------------------------------------------
-// Loops 
+// Loops - to execute a piece of code again and again
+// for ...loop 
+// print 1 to 5
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+console.log("loop ended");
+// calculate sum 1 to 5
+let sum = 0;
+for (let i = 1; i <= 5; i++) {
+    sum = sum + i;
+}
+console.log(sum);
