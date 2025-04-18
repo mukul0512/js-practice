5d9edfa1a86999006d2840c28dfb6cd3b2274663
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-18 19:44:54 +0530

    [JS-Practice] [Fundamentals] [JS] did while ...loop exaples

--- Fundamentals/JS/fundamentals.js
+++ Fundamentals/JS/fundamentals.js
@@ -4,7 +4,7 @@ console.log("Let's get started..");
 // Loops - to execute a piece of code again and again
 // for ...loop 
 // print 1 to 5
-for (let i = 0; i < 5; i++) {
+for (let i = 1; i <= 5; i++) {
     console.log(i);
 }
 console.log("loop ended");
@@ -14,3 +14,23 @@ for (let i = 1; i <= 5; i++) {
     sum = sum + i;
 }
 console.log(sum);
+
+// --------------------------------------------------------------------------
+
+// while ... loop 
+// print 1 to 5
+let i = 1;
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+// calculate sum 1 to 5
+let sum1 = 0;
+let j = 1;
+while (j <= 5) {
+    sum1 = sum1 + j;
+    j++;
+}
+console.log(sum1);
+// -------------------------------------------------------------------------------
+
