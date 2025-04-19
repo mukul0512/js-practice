c7a9e2c9e7eaa0d5781362cf1b0b1c30e5eb9a4a
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-19 15:07:26 +0530

    [JS-Practice] [Fundamentals] [JS] Arrays example

--- Fundamentals/JS/fundamentals.js
+++ Fundamentals/JS/fundamentals.js
@@ -308,3 +308,260 @@ eg. user name = "mukulkarnwal", username should be "@mukulKarnwal05"
 
 // ----------------------------------------------------------------------------------------------------------
 
+// Arrays -> Collections of items in array is a linear way as compared to object
+// What is Array? -> Array itself is an object having indices and values as compare to keys and values in an object
+// Note -> Array itself is an object type
+// Create Array
+let heroes = ["ironman", "hulk", "thor", "batman"]; // array of string
+console.log(heroes);
+console.log(typeof heroes);
+
+let marks = [96, 75, 48, 83, 66]; // array of numbers
+console.log(marks);
+console.log(typeof marks);
+
+let info = ["mukul", 86, "Haridwar"]; // array of any data type can be created
+console.log(info);
+console.log(typeof info);
+
+// Example object vs array
+// need keys to store values inside object. Tracking values by different names using keys is inconvenient as compare to array. What matters is index in an array. But there are different use cases where we need object.
+let marksOfStudents = {
+    stud1: 85,
+    stud2: 75,
+    stud3: 98,
+    stud4: 68.91
+}
+
+let studMarks = [85, 75, 98, 68.91]; // linear way to store marks of students
+// Arrays properties 
+// property -> gives just a value
+// methods -> which do some work
+console.log(studMarks);
+console.log(studMarks.length);
+
+// Array Indices vs String Indices
+// Examples of strings indices
+let str14 = "codingworkx";
+console.log(str14);
+console.log(str14.length);
+
+// Case 1
+console.log(str14[0]);
+console.log(str14[1]);
+console.log(str14[2]);
+console.log(str14[3]);
+console.log(str14[4]);
+console.log(str14[5]);
+// Case 2
+console.log(str14.charAt(0));
+console.log(str14.charAt(1));
+console.log(str14.charAt(2));
+console.log(str14.charAt(3));
+console.log(str14.charAt(4));
+console.log(str14.charAt(5));
+// Case 3
+console.log(str14.indexOf('c'));
+console.log(str14.indexOf('o'));
+console.log(str14.indexOf('d'));
+console.log(str14.indexOf('i'));
+console.log(str14.indexOf('n'));
+console.log(str14.indexOf('g'));
+
+// Examples of Arrays indices
+// Example 1
+let arr1 = ['c', 'o', 'd', 'i', 'n', 'g'];
+console.log(arr1);
+console.log(arr1.length);
+
+// Case 1
+console.log(arr1[0]);
+console.log(arr1[1]);
+console.log(arr1[2]);
+console.log(arr1[3]);
+console.log(arr1[4]);
+console.log(arr1[5]);
+// Case 2
+// console.log(arr1.charAt(0)); // err; arr1.charAt is not a function
+
+// Case 3
+console.log(arr1.indexOf('c'));
+console.log(arr1.indexOf('o'));
+console.log(arr1.indexOf('d'));
+console.log(arr1.indexOf('i'));
+console.log(arr1.indexOf('n'));
+console.log(arr1.indexOf('g'));
+
+// Example 2
+let arr2 = [5, 10, 15, 20, 25, 30];
+console.log(arr2);
+console.log(arr2.length);
+
+// Case 1
+console.log(arr2[0]);
+console.log(arr2[1]);
+console.log(arr2[2]);
+console.log(arr2[3]);
+console.log(arr2[4]);
+console.log(arr2[5]);
+// Case 2
+console.log(arr2.indexOf(0));
+console.log(arr2.indexOf(1));
+console.log(arr2.indexOf(2));
+console.log(arr2.indexOf(3));
+console.log(arr2.indexOf(4));
+
+console.log(arr2.indexOf(5));
+
+// Example 3
+let arr3 = ["Mukul", 25, 75.91, "Haridwar", 'M'];
+console.log(arr3);
+console.log(arr3.length);
+
+// Case 1
+console.log(arr3[0]);
+console.log(arr3[1]);
+console.log(arr3[2]);
+console.log(arr3[3]);
+console.log(arr3[4]);
+
+console.log(arr3[5]); // because total length is 5
+// Case 2
+console.log(arr3.indexOf('Mukul'));
+console.log(arr3.indexOf(25));
+console.log(arr3.indexOf(75.91));
+console.log(arr3.indexOf('Haridwar'));
+console.log(arr3.indexOf('M'));
+
+console.log(arr3.indexOf()); // because total length is 5
+
+// ----------------------------------------------------------------------
+
+// We can change the original array.
+// Example 
+let arr4 = [97, 85, 72, 99, 68.8];
+console.log(arr4);
+arr4[4] = 91; // possible because array is mutable
+console.log(arr4); // 68.8 is replaced with 91
+
+// -------------------------------------------------------------------
+
+// looping over an array
+// Print all elements of an array
+// Example 1 
+let supperHeroes = ["ironman", "thor", "hulk", "saktiman", "spiderman", "antman"];
+// Case 1
+console.log(supperHeroes[0]);
+console.log(supperHeroes[1]);
+console.log(supperHeroes[2]);
+console.log(supperHeroes[3]);
+console.log(supperHeroes[4]);
+console.log(supperHeroes[5]);
+// Case 2
+// using for loop
+for (let i = 0; i < supperHeroes.length; i++) {
+    console.log(supperHeroes[i]);
+}
+// Case 3
+// using while loop
+let z = 0;
+while (z < supperHeroes.length) {
+    console.log(supperHeroes[z]);
+    z++;
+}
+// Case 4
+// using do-while loop
+let y = 0;
+do {
+    console.log(supperHeroes[y]);
+    y++;
+
+} while (y < supperHeroes.length);
+
+// Note ->  We can also used for ...of loop or for ...in loop but generally we use for ...of loop to iterate over arrays.
+// Case 5
+// Example 2
+let heroNames = ["ironman", "thor", "hulk", "saktiman", "spiderman", "antman"];
+// using for ...of loop
+// iterable -> includes (strings, objects, arrays)
+// Strings -> Collection of characters.
+// Objects -> Collection of keys and values.
+// Arrays -> Collection of indices and values.
+for (let idx of heroNames) { // here val is iterable/ indices and supperHeroes is the array name
+    console.log(idx)
+}
+// Case 6
+// using for ...in loop
+for (let idx in heroNames) { // here heroNames acts like an object and idex acts like key
+    console.log(heroNames[idx]);
+}
+
+// Example 3
+let citesName = ["Haridwar", "Noida", "Bangalore", "Roorkee", "Dehradun"];
+console.log(citesName);
+// using for-of loop
+for (let elements of citesName) {
+    console.log(elements);
+}
+// using for-in loop
+for (let elements in citesName) {
+    console.log(citesName[elements]);
+}
+
+// Practice Question 1
+// For a given array with marks of students -> [85, 97, 44, 37, 76, 60]
+// Find the average marks of the entire class.
+
+let studentsMarks = [85, 97, 44, 37, 76, 60];
+// using classical for loop
+let totalSum = 0;
+for (let i = 0; i < studentsMarks.length; i++) {
+    totalSum = studentsMarks[i] + totalSum;
+}
+let avgMarks = totalSum / studentsMarks.length;
+console.log(`${totalSum}`);
+console.log(`${avgMarks}`);
+
+// using for-of loop
+let sumOfMarks = 0;
+for (let marks of studentsMarks) {
+    // sumOfMarks = sumOfMarks + marks;
+    sumOfMarks += marks;
+}
+let averageMarks = sumOfMarks / studentsMarks.length
+console.log(`${sumOfMarks}`);
+console.log(`${averageMarks}`);
+
+// Practice Question 2
+// For a given array with prices of 5 items ->
+// [250, 645, 300, 900, 50]
+// All items have an offer of 10% OFF on them. Change the array to store final price after applying offer.
+
+// using for loop 
+let pricesOfItems = [250, 645, 300, 900, 50];
+let offer = 10
+let finalPrice = 0;
+for (let i = 0; i < pricesOfItems.length; i++) {
+    let priceAfterOfferApply = pricesOfItems[i] / offer;
+    console.log(priceAfterOfferApply);
+    finalPrice = pricesOfItems[i] - priceAfterOfferApply;
+    console.log(finalPrice);
+}
+console.log(finalPrice);
+
+// using for ---of loop
+let pricesListOfItems = [250, 645, 300, 900, 50];
+let offerOnPrice = 10;
+let finalPriceOnItems = 0;
+for (let priceOfEachItem of pricesListOfItems) {
+    console.log(priceOfEachItem);
+    let priceAfterOffer = priceOfEachItem / offerOnPrice;
+    console.log(priceAfterOffer);
+    finalPriceOnItems = priceOfEachItem - priceAfterOffer;
+    console.log(finalPriceOnItems);
+}
+console.log(finalPriceOnItems);
+
+// --------------------------------------------------------------------------------------
+
+// Arrays Methods
