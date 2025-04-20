689b9f6d3f42f3f745f5d4f7b3d86f5a8c01db50
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-20 13:47:36 +0530

    [JS-Practice] [Fundamentals] [JS] created toggle button - dark/light mode using event listener

---
+++ Fundamentals/JS/toggle.js
@@ -0,0 +1,30 @@
+/*
+
+* Create a toogle button that changes the screen to dark-mode when clicked and light-mode when clicked again.
+
+*/
+
+let toggleBtn = document.querySelector("#toggleBtn");
+let body = document.querySelector("body");
+
+let currentMode = "light";
+
+const toggleHandler = () => {
+    if (currentMode === "light") {
+        currentMode = "dark";
+        // document.querySelector("body").style.backgroundColor = "black";
+        // document.querySelector("body").classList.add("dark");
+        body.classList.add("dark");
+        body.classList.remove("light");
+    }
+    else {
+        currentMode = "light";
+        // document.querySelector("body").style.backgroundColor = "white";
+        // document.querySelector("body").classList.add("light");
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+    console.log(currentMode);
+}
+
+toggleBtn.addEventListener("click", toggleHandler);
