381d9e10001893c00e6d9899b7f2ebea79c0e632
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-20 21:52:14 +0530

    [JS-Practice] [Day13] promise chaining

--- Day13/script.js
+++ Day13/script.js
@@ -216,3 +216,63 @@ finalPromise.catch((err) => {
 
 // let receivedPromise = getNewDataFromDatabase(123);
 // console.log(receivedPromise);
+
+// ----------------------------------------------------------------------------------------------
+
+/*
+
+* Promise Chain
+
+
+*/
+
+function asyncFunc1() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("data1");
+            resolve("success");
+        }, 4000);
+    });
+}
+
+function asyncFunc2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("data2");
+            resolve("success");
+        }, 4000);
+    });
+}
+
+// console.log("fetching data1 ...");
+// let p1 = asyncFunc1();
+// p1.then((res) => {
+//     console.log(res);
+// });
+
+// console.log("fetching data2 ...");
+// let p2 = asyncFunc2();
+// p2.then((res) => {
+//     console.log(res);
+// });
+// -----------------------------------------------------------------------
+// promise chaining -> ek then k andar dusra promise resolve ho.
+// console.log("fetching data1 ...");
+// let p1 = asyncFunc1();
+// p1.then((res) => {
+//     console.log(res);
+//     console.log("fetching data2 ...");
+//     let p2 = asyncFunc2();
+//     p2.then((res) => {
+//         console.log(res);
+//     });
+// });
+// ------------------------------------------------------------
+console.log("fetching data1 ...");
+asyncFunc1().then((res) => {
+    console.log(res);
+    console.log("fetching data2 ...");
+    asyncFunc2().then((res) => {
+        console.log(res);
+    });
+});
