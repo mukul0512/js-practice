7cef1fbe8662bb4daee5ce5405fb1caf5f02c897
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-04-20 20:12:34 +0530

    [JS-Practice] [Day13] callback hell example

--- Day13/script.js
+++ Day13/script.js
@@ -69,7 +69,63 @@ setTimeout(helloWorld, 3000);
 
 /*
 
-* callback hell 
-* 
+* callback hell
+* Nested callbacks stacked below one another forming a pyramid structure. (Pyramid of Doom)
+* This style of programming becomes difficult to understand and manage.
 
 */
+
+// nesting if ...else example
+let age = 19;
+if (age >= 18) {
+    if (age >= 60) {
+        console.log("senior");
+    } else {
+        console.log("middle");
+    }
+} else {
+    console.log("child");
+}
+
+// --------------------------------------------------------------
+
+// nesting loop example
+for (let i = 0; i < 5; i++) {
+    let str = "";
+    for (let j = 0; j < 5; j++) {
+        str = str + j;
+    }
+    console.log(i, str);
+}
+// ----------------------------------------------------------------------------------------------------------------
+// nesting callback known as Callback Hell form (Pyramid of Doom)
+
+function getData(dataId, getNextData) { // assume it takes 2 sec to search dataId from the database
+    // console.log("data", dataId);
+    setTimeout(() => {
+        console.log("data", dataId);
+    }, 2000);
+}
+
+// getData(2, getNextData) // 2s
+// getData(3, getNextData) // 2s
+// getData(4, getNextData) // 2s
+
+// coming from database
+//data1 
+//data2 
+//data3 
+//data4 
+
+// Example of nesting callback known as Callback Hell (Pyramid of Doom)
+getData(1, () => { // 2s here data 1 is came and getting of data 2 through callback
+    console.log("getting data2 ...");
+    getNextData(2, () => { // 2s callback
+        console.log("getting data3 ...");
+        getData(3, () => { // 2s callback
+            console.log("getting data4 ...");
+            getData(4);
+        });
+    })
+})
+
