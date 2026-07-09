e8be7a6e8a2c03b145a989da9f2ea5480f38813c
Mukul Karnwal <karnwal.mukul@gmail.com>
2026-07-09 10:21:18 +0530

    calc. area, circumf. and diameter of 4 circles

--- InteviewQuestions/script.js
+++ InteviewQuestions/script.js
@@ -1,1 +1,27 @@
 console.log("Higher order functions");
+
+const radius = [3, 1, 2, 4];
+
+const area = function (radius) {
+  return Math.PI * radius * radius;
+};
+
+const circumference = function (radius) {
+  return 2 * Math.PI * radius;
+};
+
+const diameter = function (radius) {
+  return 2 * radius;
+};
+
+const calculate = function (radius, logic) {
+  const output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(logic(radius[i]));
+  }
+  return output;
+}
+
+console.log("Area of 4 circles:", calculate(radius, area));
+console.log("Circumferences of 4 circles:", calculate(radius, circumference));
+console.log("Diameters of 4 circles:", calculate(radius, diameter));
